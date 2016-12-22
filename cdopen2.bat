1938bc650742746e75ea06857a2916ea8b13d24e
Bryan Barnes <hooby3d@gmail.com>
2016-12-21 21:53:33 -0500

    Update cdopen2.bat

--- cdopen2.bat
+++ cdopen2.bat
@@ -1,1 +1,2 @@
+rem from http://superuser.com/questions/972447/how-can-i-eject-a-cd-via-the-cmd/972448
 powershell "(new-object -COM Shell.Application).NameSpace(17).ParseName('D:').InvokeVerb('Eject')"
